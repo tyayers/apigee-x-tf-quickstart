57a6f3baf8d4371e2b134b11625120bea983abe5
tyayers <tyler.ayers@gmail.com>
2024-04-18 19:05:59 +0200

    updated correct tfvar calls in apply and destroy

--- 1_apply.sh
+++ 1_apply.sh
@@ -1,6 +1,6 @@
 cd tf
 
 terraform init
-terraform apply -var "project_id=$PROJECT_ID" -var "billing_account=$BILLING_ID" -var "region=$REGION" -var "add_user=$GCP_ADD_USER" -var "project_create=true"
+terraform apply --var-file=./x-eval.dev.tfvars
 
 cd ..
--- 2_destroy.sh
+++ 2_destroy.sh
@@ -1,5 +1,5 @@
 cd tf
 
-terraform destroy -var "project_id=$PROJECT_ID" -var "billing_account=$BILLING_ID" -var "region=$REGION" -var "add_user=$GCP_ADD_USER" -var "project_create=true"
+terraform destroy --var-file=./x-eval.dev.tfvars
 
 cd ..
