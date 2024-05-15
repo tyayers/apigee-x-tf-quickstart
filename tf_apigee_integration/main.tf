e45864986598a904181eeac61a65f3d8ad1aacca
tyayers <tyler.ayers@gmail.com>
2024-05-15 15:30:14 +0200

    updated scripts to be easier to run

--- tf_apigee_integration/main.tf
+++ tf_apigee_integration/main.tf
@@ -139,12 +139,13 @@ module "nb-psc-l7xlb" {
 
 module "apigee_addons" {
   source     = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/apigee"
-  project_id = module.apigee-x-core.project_id
+  project_id = module.project.project_id
   addons_config = {
     monetization = true
     api_security = true
     advanced_api_ops = true
   }
+  depends_on = [ module.apigee-x-core ]
 }
 
 # This was necessary to give the project time to apply the 
